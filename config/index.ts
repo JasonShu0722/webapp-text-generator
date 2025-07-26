547224d039f15c7d33461cbb28bc259a9f13d548
JasonShu0722 <122194334+JasonShu0722@users.noreply.github.com>
2025-07-27 00:37:00 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -4,20 +4,6 @@ export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 'true'
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
-  copyright: '',
-  privacy_policy: '',
-  default_language: 'en-US',
-}
-
-export const API_PREFIX = '/api'
-
-export const LOCALE_COOKIE_NAME = 'locale'
-
-export const DEFAULT_VALUE_MAX_LEN = 48
-
-export const APP_INFO: AppInfo = {
   title: 'Chat APP',
   description: '',
   copyright: '',
@@ -27,3 +13,11 @@ export const APP_INFO: AppInfo = {
 
 export const isShowPrompt = true
 export const promptTemplate = ''
+
+export const API_PREFIX = '/api'
+
+export const LOCALE_COOKIE_NAME = 'locale'
+
+export const DEFAULT_VALUE_MAX_LEN = 48
+
+
